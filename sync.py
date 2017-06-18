b987054a7307cd6447caff89ba404f4581cd8587
musray <churui.ctec@gmail.com>
2017-06-18 13:55:41 +0800

    basic sync function completed

--- sync.py
+++ sync.py
@@ -4,10 +4,14 @@ import os
 import csv
 from operator import itemgetter
 
+# ---------------------------------------------- #
+#                 Abstraction
 # 这个文件用来同步Firmnet上Send和Receive的偏移地址
 # 在Firmnet上，Send类型点的偏移地址，是手工指定的，
 # 而Receive类型点的偏移地址，是从Send继承来的
 # 手工填写的话太麻烦，还容易错，所以写了个脚本实现。
+# ---------------------------------------------- #
+
 
 
 def file_validation(csv_list):
@@ -56,36 +60,69 @@ def file_validation(csv_list):
     return netdev_list, firmnet_list
 
 
-# def collect_send(filelist):
-#     # 分别返回datalink和firmnet的所有send信号
-#     # 两个list
-#     datalink_send_list = []
-#     firmnet_send_list = []
-#
-#     for f in filelist:
-#         if 'netdev' in f:
-#             # 收集datalink的send信号（整行保存）
-#             csv_reader = csv.read(f, 'r', encoding='gbk')
-#             next(reader) # 甩掉首行
-#             for row in list(csv_reader):
-#                 if row[] # TODO 用第几列进行判断？
-#
-#         elif 'download' in f:
-#             # 收集firmnet的send信号（整行保存）
-#
-#     return datalink_send_list, firmnet_send_list
+def get_offset_value(name, ring, data):
+    # 此函数只在sync_offset中被调用，其中：
+    #   - name: string，信号名
+    #   - ring: int, 环网号，1，2，3，4
+    #   - data: 三维数组，环网数据
+
+    #
+    # DEBUG
+    #
+    # print(name, ring)
+
+    warning_info = ['Safety System BUS(offset_download_1.csv)', 'Safety BUS Train A(offset_download_2.csv)', \
+                    'Safety BUS Train B(offset_download_3.csv)', 'HM Data BUS(offset_download_4.csv)']
+
+    # 下面这个list comprehension的意图：找到 name 所在的行
+    # 1. 遍历data中第ring-1个元素所对应的二维数组，找到 name 所在的环网的数据
+    # 2. 如果row[2]点名 == name，并且row[1]的内容为send或者dss，
+    # 3. 把这个row返回。（根据点表的填写规则，只能有唯一的row被返回）
+    target_row = [row for row in data[ring-1] if row[2].lower() == name.lower() and
+                  (row[1].lower() == 'send' or row[1].lower() == 'dss')]
+
+    # 做一个校验，如果返回的target_row为空，说明相应的环网csv文件不在 sync 文件夹里（产生这种情况的最可能原因）
+    if not target_row:
+        input('错误。请检查 sync 中是否包含 %s 的相应文件。\n按任意键退出...' % warning_info[ring - 1])
+        sys.exit(1)
+
+    # 把offset值强制成int返回
+    return int(target_row[0][6].strip())
 
 
-def sync_offset(csvfile):
-    # with open(csvfile, 'r', encoding='gbk') as in_file, \
-    #      open(newfile, 'w', encoding='gbk') as out_file:
-    pass
+def sync_offset(netdev_file, firmnet_data):
+    path, basename = os.path.split(netdev_file)
+    new_file = os.path.join(path, 'sync_' + basename)
+    with open(netdev_file, 'r', encoding='gbk') as in_file, \
+         open(new_file, 'w', encoding='gbk') as out_file:
+
+        # 把csv的内容读出来
+        netdev_list = list(csv.reader(in_file))
+
+        # 把需要新建的文件，用csv writer打开
+        writer = csv.writer(out_file)
+
+        # 遍历所有行，如果：
+        # 1. row[10]中有"环点"字样，则说明该行是环网的点，看倒数第二位的环号，赋值给变量（校验环网的csv是不是在sync文件夹中）
+        # 2. row[5]中的设备类型如果是send，则：用row[1]的点名去firmnet_data里找相应的点，偏移地址写到netdev_list的row[11]中
+        for row in netdev_list:
+            # 是环网上的点？
+            if '环点' in row[10]:
+                net_ring = int(row[10].strip()[-2])
+                signal_name = row[1]
+                # 调用get_offset_value
+                offset_value = get_offset_value(signal_name, net_ring, firmnet_data)  # 返回值是int
+                row[11] = offset_value
+
+        # 把需要新建的文件，用csv writer打开
+        writer = csv.writer(out_file)
+        writer.writerows(netdev_list)
 
 
 def get_firmnet_data(firmnet_files):
-    # 把firmnet文件挨个打开，存到一个二维list中
-    # 问题：如果只有二维list，如何确保firmnet1, 2, 3, 4的数据在二维数组中的排列顺序？
-    # 如果不用二维数组，用dict，也许能比较好的解决这个问题
+    # 把firmnet文件挨个打开，存到一个三维list中
+    # 问题：如果只有三维list，如何确保firmnet1, 2, 3, 4的数据在三维数组中的排列顺序？
+    # 如果不用三维数组，用dict，也许能比较好的解决这个问题
     firmnet_data = [[], [], [], []]
     for firmnet_file in firmnet_files:
         with open(firmnet_file, 'r', encoding='gbk') as f_f:
@@ -100,7 +137,7 @@ def get_firmnet_data(firmnet_files):
             except ValueError:
                 input('%s 文件命名错误，确认后重新运行程序。\n按任意键退出...')
                 sys.exit(1)
-            # sequence决定了data在firmnet_data这个二维list中的顺序
+            # sequence决定了data在firmnet_data这个三维list中的顺序
             firmnet_data[sequence - 1] = data
 
     return firmnet_data
@@ -122,31 +159,50 @@ def ttest_get_firmnet_data(data):
 
 def main():
     # 基本思路
-    # 1. 用户把带有offset的NETDEV和download，都放到sync中
-    # 2. 打开所有的download文件，把所有行都读出来，每一个文件存到一个list中
+    # 1. [DONE]用户把带有offset的NETDEV和download，都放到sync中
+    # 2. [DONE]打开所有的download文件，把所有行都读出来，每一个文件存到一个list中
     # 3. 遍历所有的netdev.csv：
     #   - 找到环网的点
-    #   - 如果是send/dss，看属于哪个环网，根据点名去对应环网的数据中查offset，写到最后一列
-    #   - 如果是recv/dsr，TODO
-    # TODO 确认：四个环网上会不会有重名的点？（暂时按没有重名点开发）
+    #   - 对于环网上四个类型的点，dss/dsr/send/recv，都按照点名和环网号，去对应的环网csv中去找send或dss的点
 
+    #
+    # 第1步
     # 检查是否有 sync 文件夹
+    #
     if not os.path.isdir('sync'):
         # 没有的话新建一个，并退出
         os.mkdir('sync')
         input('当前路径下没有 sync 文件夹，已经为你新建。')
         sys.exit(1)
 
+    #
+    # 第2步
     # 遍历 sync 内的所有文件，分别找到offset_netdev和offset_download
-    # ，而且都是绝对路径
+    # ，两个files中已经是 [ !绝对路径! ]
+    #
     netdev_files, firmnet_files = file_validation(os.listdir('./sync'))
 
-    # 生成一个存有firmnet.csv的二维数组
+    #
+    # 第3步
+    # 生成一个存有firmnet.csv数据的三维数组
     # 数组中从0到3分别为Safety System BUS, Safety BUS TrainA, Safety_BUS TrainB 和 HM Data BUS
+    #
     firmnet_data = get_firmnet_data(firmnet_files)
 
+    #
+    # 第4步
+    # 在netdev.csv中，逐个填写
+    # 找到其中环网的点，从firmnet_data中找到其对应的点名，拷贝offset值并粘贴
+    #
+    for netdev_file in netdev_files:
+        # 先cue一下焦急等待的群众：
+        print('%s 处理中，请稍候...' % netdev_file)
+        sync_offset(netdev_file, firmnet_data)
+        print('已完成！')
+
     # Test and DEBUG
-    ttest_get_firmnet_data(firmnet_data)
+    # ttest_get_firmnet_data(firmnet_data)
+    input('%d 个文件已成功添加偏移地址。\n按任意键退出...' % len(netdev_files))
 
 if __name__ == '__main__':
     main()
