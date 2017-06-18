a87bc12c5fbf98158327520610be39c117ef8cb7
musray <churui.ctec@gmail.com>
2017-06-18 11:18:22 +0800

    add functions <get all firmnet data> in sync.py; works fine

--- sync.py
+++ sync.py
@@ -7,101 +7,146 @@ from operator import itemgetter
 # 这个文件用来同步Firmnet上Send和Receive的偏移地址
 # 在Firmnet上，Send类型点的偏移地址，是手工指定的，
 # 而Receive类型点的偏移地址，是从Send继承来的
-# 手工继承（填写）的话太麻烦，还容易错，所以写了个脚本实现。
+# 手工填写的话太麻烦，还容易错，所以写了个脚本实现。
 
 
 def file_validation(csv_list):
-    # 在正式处理csv文件之前，进行一系列校验
+    # csv_list 是 sync 文件夹内所有文件的清单
+    # ，我们要把firmnet和netdev两种文件找出来，分别放入各自的list
+    # ，并将其return(return的是绝对路径)
+
+    firmnet_list = []
+    netdev_list = []
+
+    #
     # 校验1： 如果文件夹中没有任何文件，则直接退出
+    #
     if len(csv_list) == 0:
         input('在sync文件夹中没有待处理的文件，按任意键退出。。。')
         sys.exit(1)
 
-    # 如果有文件，检查文件名中是否有download或者netdev字符串
-    result = []
+    #
+    # 校验2：文件夹中必须同时有netdev何download文件
+    # 按关键字，把csv文件分为firmnet和netdev两类
+    #
     for csv_file in csv_list:
-        if 'download' in csv_file.lower():
-            result.append('has_download')
-        if 'netdev' in csv_file.lower():
-            result.append('has_netdev')
+        if 'offset_download' in csv_file.lower()[0:15]:
+            # 生成绝对路径，append到list中
+            file_abspath = os.path.join(os.getcwd(), 'sync', csv_file)
+            firmnet_list.append(file_abspath)
+        elif 'offset_netdev' in csv_file.lower()[0:13]:
+            # 生成绝对路径，append到list中
+            file_abspath = os.path.join(os.getcwd(), 'sync', csv_file)
+            print('catch it %s' % file_abspath)
+            netdev_list.append(file_abspath)
 
-    # 如果文件中不存在环网点表，只对datalink的recv进行同步
-    if not 'has_download' in result:
-        print('文件夹中没有环网文件download，无法同步环网类的recv信号偏移地址，只对datalink点进行同步，请知晓。')
+    #
+    # 校验3：firmnet和netdev文件必须同时存在
+    # 原因：所谓同步功能，实际上就是把firmnet表里生成的send和dss类偏移地址，相应拷贝到netdev里去
+    # ，所以两者必须都存在，才有继续执行的意义。
+    #
+    if len(firmnet_list) == 0:
+        input('sync 文件夹中没有环网文件sync_download.csv，无法执行环网偏移地址同步。\n按任意键退出...')
+        sys.exit(1)
     # 如果文件中不存在netdev类型点表，相当于不存在
-    if not 'has_netdev' in result:
-        input('文件夹中没有netdev类的通信清单，程序不再执行，请按任意键退出。。。')
+    if len(netdev_list) == 0:
+        input('文件夹中没有netdev类的通信清单，无法执行环网偏移地址同步。\n按任意键退出...')
         sys.exit(1)
 
-
-def abs_path(filelist):
-    # 为filelist中所有文件生成绝对路径文件名
-    abspath_filelist = []
-    for f in filelist:
-        f_abspath = os.path.join(os.getcwd(), 'sync', f)
-        abspath_filelist.append(f_abspath)
-
-    return abs_path_filelist
+    return netdev_list, firmnet_list
 
 
-def collect_send(filelist):
-    # 分别返回datalink和firmnet的所有send信号
-    # 两个list
-    datalink_send_list = []
-    firmnet_send_list = []
+# def collect_send(filelist):
+#     # 分别返回datalink和firmnet的所有send信号
+#     # 两个list
+#     datalink_send_list = []
+#     firmnet_send_list = []
+#
+#     for f in filelist:
+#         if 'netdev' in f:
+#             # 收集datalink的send信号（整行保存）
+#             csv_reader = csv.read(f, 'r', encoding='gbk')
+#             next(reader) # 甩掉首行
+#             for row in list(csv_reader):
+#                 if row[] # TODO 用第几列进行判断？
+#
+#         elif 'download' in f:
+#             # 收集firmnet的send信号（整行保存）
+#
+#     return datalink_send_list, firmnet_send_list
 
-    for f in filelist:
-        if 'netdev' in f:
-            # 收集datalink的send信号（整行保存）
-            csv_reader = csv.read(f, 'r', encoding='gbk')
-            next(reader) # 甩掉首行
-            for row in list(csv_reader):
-                if row[] # TODO 用第几列进行判断？
-
-        elif 'download' in f:
-            # 收集firmnet的send信号（整行保存）
-
-    return datalink_send_list, firmnet_send_list
 
 def sync_offset(csvfile):
     # with open(csvfile, 'r', encoding='gbk') as in_file, \
     #      open(newfile, 'w', encoding='gbk') as out_file:
     pass
-        
 
+
+def get_firmnet_data(firmnet_files):
+    # 把firmnet文件挨个打开，存到一个二维list中
+    # 问题：如果只有二维list，如何确保firmnet1, 2, 3, 4的数据在二维数组中的排列顺序？
+    # 如果不用二维数组，用dict，也许能比较好的解决这个问题
+    firmnet_data = [[], [], [], []]
+    for firmnet_file in firmnet_files:
+        with open(firmnet_file, 'r', encoding='gbk') as f_f:
+            data = list(csv.reader(f_f))
+
+            # 尝试在文件名的倒数第5位获取数字，分别代表一种firmnet环网：1：SSB；2：SB_A；3：SB_B；4：HM
+            try:
+                sequence = int(firmnet_file[-5])
+                if not sequence in [1, 2, 3, 4]:
+                    input('%s 文件命名错误，文件名最后一位必须是1-4的数字。请检查文件名后重新运行程序。\n按任意键退出...')
+
+            except ValueError:
+                input('%s 文件命名错误，确认后重新运行程序。\n按任意键退出...')
+                sys.exit(1)
+            # sequence决定了data在firmnet_data这个二维list中的顺序
+            firmnet_data[sequence - 1] = data
+
+    return firmnet_data
+
+
+def ttest_get_firmnet_data(data):
+    print('+-----------------+')
+    print('测试内容：')
+    source = '24852'
+    target = data[0][8945][6]
+    print('\t希望值是 %s，实际得到 %s' % (source, target))
+    print('测试结果：')
+    if target == source:
+        print('\t测试通过！')
+    else:
+        print('\t测试失败')
+    print('+-----------------+')
 
 
 def main():
-    # TODO 基本思路
-    # 用户把需要填写的csv（target），和已经填写好Send偏移地址的csv文件（source）放在同一文件夹内
-    # 先open所有上述文件
-    # 在需要填写offset的文件中，遍历所有行
-    # 如果这一行是环网点、且点类型是Receive，那么:
-    #   - 根据点名，在source中找对应点名，且校验找到的点是否为Send类型
-    #   - 如果找到的点满足上条要求，则把source中的offset写入到target的相应位置上
+    # 基本思路
+    # 1. 用户把带有offset的NETDEV和download，都放到sync中
+    # 2. 打开所有的download文件，把所有行都读出来，每一个文件存到一个list中
+    # 3. 遍历所有的netdev.csv：
+    #   - 找到环网的点
+    #   - 如果是send/dss，看属于哪个环网，根据点名去对应环网的数据中查offset，写到最后一列
+    #   - 如果是recv/dsr，TODO
+    # TODO 确认：四个环网上会不会有重名的点？（暂时按没有重名点开发）
 
+    # 检查是否有 sync 文件夹
     if not os.path.isdir('sync'):
+        # 没有的话新建一个，并退出
         os.mkdir('sync')
+        input('当前路径下没有 sync 文件夹，已经为你新建。')
+        sys.exit(1)
 
-    csvfiles = os.listdir('./sync')
-    file_validation(csvfiles)
+    # 遍历 sync 内的所有文件，分别找到offset_netdev和offset_download
+    # ，而且都是绝对路径
+    netdev_files, firmnet_files = file_validation(os.listdir('./sync'))
 
-    # 生成所有绝对路径csv文件的list
-    # 同时生成一个所有send信号的数据体（TODO 什么数据类型？）
-    csvfiles_abspath = abs_path(csvfiles)  # return a list
-    send_signals_pool = collect_send(csvfiles_abspath) # return a list of lists
+    # 生成一个存有firmnet.csv的二维数组
+    # 数组中从0到3分别为Safety System BUS, Safety BUS TrainA, Safety_BUS TrainB 和 HM Data BUS
+    firmnet_data = get_firmnet_data(firmnet_files)
 
-    for csvfile in csvfiles:
-        # 如果文件是csv格式，而且文件名前4位不是sync开头，则对该文件进行处理
-        if csvfile[-3: ].lower() == 'csv' and csvfile[ :4].lower() != 'sync':
-            # 生成
-            in_file_path = os.path.join(os.getcwd(), 'offset', csvfile)
-            out_file_path = os.path.join(os.getcwd(), 'offset', 'offset_' + csvfile)
-            csv_handler(in_file_path, out_file_path)
-            print(os.path.basename(csvfile) + ' 处理完毕')
-    input('按任意键退出...')
-        sync_offset(csvfile)
-
+    # Test and DEBUG
+    ttest_get_firmnet_data(firmnet_data)
 
 if __name__ == '__main__':
     main()
